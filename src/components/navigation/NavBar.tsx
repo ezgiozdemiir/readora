4ac2ccf58a4d3a5e08adf10366821d9006ea9640
Ezgi Özdemir <ezgi.karaagac@outlook.com>
2025-05-22 14:20:28 +0300

    RDR-5: Create navigation bar

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Container, Group, Button } from '@mantine/core';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/Readora.png'
+import './NavBar.scss'
 
 const NavBar: React.FC = () => {
   return (
-    <Container>
+    <Container className='navbar'>
       <Group >
+        <img src={logo} alt="readora" />
         <Button component={Link} to="/" variant="light" color="blue">Home</Button>
         <Button component={Link} to="/profile" variant="light" color="blue">Profile</Button>
         <Button component={Link} to="/login" variant="light" color="blue">Login</Button>
